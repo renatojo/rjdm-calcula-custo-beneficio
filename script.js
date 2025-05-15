7ad0d1b7927d81b2ea1f125ce4bd580f075314c7
renatojo <renatojose.mo@gmail.com>
2025-05-14 21:53:19 -0300

    alteção no html e css, adicionado conteúdo no script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('btnCalc').addEventListener('click',(event)=>{
+    event.preventDefault();
+    const precoUnitario = document.getElementById('preco-unitario').value;
+    const quantidade = document.getElementById('quantidade').value;
+    const desconto = document.getElementById('desconto').value;
+
+    const vlTotal = precoUnitario * quantidade;
+    const vlDesconto = vlTotal * (desconto/100);
+    const resultado = vlTotal - vlDesconto;
+    const divResultado = document.getElementById("result");
+
+    divResultado.innerHTML = `<h3>${resultado}</h3>`
+})
